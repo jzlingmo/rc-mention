ff310016d6aadabd130b2fbddc8f7b2732c807ce
Vijay <jzlingmo@gmail.com>
2016-04-11 17:38:21 +0800

    v2 init

--- src/rangy.js
+++ src/rangy.js
@@ -1,76 +1,18 @@
 // todo if google rangy exists should extend that
-var rangy = {
-    _bookmarkId: 0,
-    getSelection(){
-        return window.getSelection();
-    },
-    createRange (){
-        return document.createRange();
-    },
-    createBookmark2(range, serializable){
-        var startNode, endNode;
-        var baseId;
-        var clone;
-        var collapsed = range.collapsed;
-        var prefix = '__bookmark__';
 
-        startNode = document.createElement('span');
-        startNode.style.display = 'none';
-
-        if (serializable) {
-            baseId = prefix + (this._bookmarkId++);
-
-            startNode.setAttribute('id', baseId + ( collapsed ? 'C' : 'S' ));
+var myrangy;
+if(window.rangy){
+    window.rangy.init();
+    myrangy = window.rangy
+}else{
+    myrangy = {
+        getSelection(){
+            return window.getSelection();
+        },
+        createRange (){
+            return document.createRange();
         }
+    };
+}
 
-        if (!collapsed) {
-            endNode = startNode.cloneNode(true);
-
-            if (serializable) {
-                endNode.setAttribute('id', baseId + 'E');
-            }
-            // insert end node
-            clone = range.cloneRange();
-            clone.collapse(false);
-            clone.insertNode(endNode);
-        }
-
-        // insert start node
-        clone = range.cloneRange();
-        clone.collapse(true);
-        clone.insertNode(startNode);
-
-        // reset range position
-        if (endNode) {
-            range.setEndBefore(endNode);
-        }
-        range.setStartAfter(startNode);
-
-        return {
-            startNode: serializable ? baseId + ( collapsed ? 'C' : 'S' ) : startNode,
-            endNode: serializable ? baseId + 'E' : endNode,
-            serializable: serializable,
-            collapsed: collapsed
-        }
-    },
-
-    moveToBookmark2(bookmark, range){
-        var serializable = bookmark.serializable,
-            startNode = serializable ? document.getElementById(bookmark.startNode) : bookmark.startNode,
-            endNode = serializable ? document.getElementById(bookmark.endNode) : bookmark.endNode;
-
-        range.setStartAfter(startNode);
-        startNode.remove();
-
-        if (endNode) {
-            range.setEndBefore(endNode);
-            endNode.remove();
-        } else {
-            range.collapse(true);
-        }
-        return range
-    }
-};
-
-
-module.exports = rangy;
+module.exports = myrangy;
